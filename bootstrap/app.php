b0f225e435c9e7e29db1d0718fcad5e7d3fc61ec
Sanjida Islam <61589353+sanjida1621@users.noreply.github.com>
2026-02-20 23:42:52 +0600

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
@@ -31,3 +31,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// Only override storage path on Vercel
+if (!empty($_ENV['VERCEL'])) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+return $app;
