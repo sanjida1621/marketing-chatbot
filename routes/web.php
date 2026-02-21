09399f9b77fb264287541d00a24b9d17f793549b
Sanjida Islam <61589353+sanjida1621@users.noreply.github.com>
2026-02-21 13:01:50 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,15 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return "The server is alive. If you see this, the 302 is gone.";
-});
-
 // Route::get('/', function () {
-//     return auth()->check()
-//         ? redirect()->route('posts.index')
-//         : redirect()->route('login');
-// })->name('home');
+//     return "The server is alive. If you see this, the 302 is gone.";
+// });
+
+Route::get('/', function () {
+    return auth()->check()
+        ? redirect()->route('posts.index')
+        : redirect()->route('login');
+})->name('home');
 
 
 Route::get('dashboard', function () {
